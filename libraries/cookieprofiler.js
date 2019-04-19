5997b173b0c254f3fd9c780fd1933f176f730ada
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2019-04-19 11:24:58 -0400

    Update cookieprofiler.js

--- libraries/cookieprofiler.js
+++ libraries/cookieprofiler.js
@@ -1,4 +1,16 @@
 var statarr = [];
 setInterval(function() {
-  statarr.push({});
+  var sobj = {
+    time: Date.now(),
+    cookiesInBank: Game.cookies,
+    cookiesEarned: Game.cookiesEarned,
+    cookiesPs: Game.cookiesPs,
+    buffs: {},
+  };
+  var okbuf = Object.keys(Game.buffs);
+  for (var i in okbuf) {
+    var k = okbuf[i];
+    buffs[k] = { time: Game.buffs[k].time, multCps: Game.buffs[k].multCps };
+  }
+  statarr.push(JSON.stringify(sobj));
 }, 1000);
