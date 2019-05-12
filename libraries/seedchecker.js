e3a9ca036e47caa8de1218c6996b28bde74652e3
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2019-05-12 10:54:07 -0400

    Update seedchecker.js

--- libraries/seedchecker.js
+++ libraries/seedchecker.js
@@ -4,12 +4,11 @@ sToSeed = function(num) {
   for (;pc >= 0; pc--) {
     str += sChars[Math.floor((num / sChars.length ** pc) % 26)];
   }
-  return s.padStart(5, sChars[0]);
+  return str.padStart(5, sChars[0]);
 };
 sToNum = function (str) {
   var num = 0, pc = str.length - 1;
   for (;pc >= 0; pc--) {
-    console.log([pc, sChars.indexOf(str[str.length - pc - 1]), sChars.length ** pc]);
     num += sChars.indexOf(str[str.length - pc - 1]) * sChars.length ** pc;
   }
   return num;
