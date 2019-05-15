545cb4846b4db0cb21867c1e76d6264af5588bc6
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2019-05-15 18:06:20 -0400

    Update beautify.js

--- libraries/beautify.js
+++ libraries/beautify.js
@@ -1,5 +1,5 @@
 // function to determine place value of beautify
-Beautifyp = () => Math.max(Math.floor(Math.log10(6900.638e45 / Game.cookiesPs) / 3) * 3, 6);
+Beautifyp = (v) => Math.max(Math.floor(Math.log10(v / Game.cookiesPs) / 3) * 3, 6);
 Beautifyl = ['million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion', 'quindecillion', 'sexdecillion', 'septendecillion', 'octodecillion', 'novemdecillion', 'vigintillion']; // prefixes
 // adds commas to string
 function BeautifyC(num) {
@@ -13,7 +13,7 @@ function Beautify(num) {
   if (num < 0) return '-' + Beautify(-num);
   if (num < 1000) return num + '';
   if (num < 1000000) return BeautifyC(num);
-  var pv = Beautifyp();
+  var pv = Beautifyp(num);
   var sec = Math.floor(Math.log10(num)/3-2);
   var sed = num / Math.pow(10, sec * 3 + 6);
   sed = Math.floor(sed * Math.pow(10, pv)) / Math.pow(10, pv);
