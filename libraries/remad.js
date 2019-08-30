3014c11dda9de5cb9832a0ff892ed402539a9b34
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2019-08-30 09:56:55 -0400

    Update remad.js

--- libraries/remad.js
+++ libraries/remad.js
@@ -1,6 +1,6 @@
 // bookmarklet:
 // javascript:(function(){if(window.c284lib_remad)c284lib_remad();else document.body.appendChild(document.createElement('script')).src='https://coolguy284.github.io/libraries/remad.js';})();
 function c284lib_remad() {
-  document.getElementsByTagName('iframe').forEach(x => { if(x.id.includes('ads')) x.remove(); })
+  Array.from(document.getElementsByTagName('iframe')).forEach(x => { if(x.id.includes('ads')) x.remove(); })
 }
 c284lib_remad();
